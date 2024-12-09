f67c35205a44e595e32a8f55eb61f19bee0d4dd8
unknown <singhchauhangautam@gmail.com>
2024-12-09 18:18:26 +0530

    api-added-bug-fixes-09-12-24

--- head/engine/ajax/ajax_manage_enquiry.php
+++ head/engine/ajax/ajax_manage_enquiry.php
@@ -543,7 +543,120 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH
 
 						$enquiry_cc_email = getENQUIRYFORM_NEEDFOR_LIST($need_for, 'email_label');
 
-                $message_template = '<h1>Test</h1>';
+                $message_template = '<!DOCTYPE html>
+				                                    <html lang="en">
+				                                    <head>
+				                                        <meta charset="UTF-8">
+				                                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+				                                        <title>SIDBI : Online Enquiry</title>
+				                                        <style>
+				                                            /* Reset some default styles */
+				                                            body,
+				                                            h1,
+				                                            h2,
+				                                            p {
+				                                                margin: 0;
+				                                                padding: 0;
+				                                            }
+				                                        </style>
+				                                    </head>
+				                                    <body style="font-family: "DM Sans", sans-serif;background-color: #f5f5f5;padding-top: 30px;">
+				                                        <div class="email-container" style="width: 80%; max-width: 600px; margin: 0 auto; background-color: #fff; padding: 20px; border-radius: 10px; box-shadow: 0 0 20px rgba(0, 0, 0, 0.1); margin-top: 20px;margin-bottom: 30px;border: 1px solid lightgray;">
+				                                            <div class="email-header" style="text-align: center;">
+				                                                <h2 style="color: #484B49;">SIDBI : Online Enquiry</h2>
+				                                            </div>
+				                                            <table style="width: 100%; border-collapse: collapse;">
+				                                                <tr>
+				                                                    <td>
+				                                                        <div align="center">
+				                                                            <!-- Content Details Table -->
+				                                                            <table width="100%" style="border-collapse: collapse; margin-top: 20px;">
+				                                                                <tr style="background: #f1f7f9;">
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 7.5pt;" colspan="2">
+				                                                                        <p style="text-align: center; color: black;">
+				                                                                            <a href="https://sidbi.in/" target="_blank" style="color: black; text-decoration: none;">
+				                                                                                <img width="150" src="' . SEOURL . '/assets/front/images/enhi.jpg" alt="Logo">
+				                                                                            </a>
+				                                                                        </p>
+				                                                                    </td>
+				                                                                </tr>
+				                                                                <tr>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt; text-align: left;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;"><b>Reference Number</b></p>
+				                                                                    </td>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt; text-align: left;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;">' . $ENQUIRY_REF_NO . '</p>
+				                                                                    </td>
+				                                                                </tr>
+				                                                                <tr>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;"><b>Full Name</b></p>
+				                                                                    </td>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;">' . $name . '</p>
+				                                                                    </td>
+				                                                                </tr>
+				                                                                <tr>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;"><b>Email ID</b></p>
+				                                                                    </td>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;">' . $emailid . '</p>
+				                                                                    </td>
+				                                                                </tr>
+				                                                                <tr>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;"><b>Contact Number</b></p>
+				                                                                    </td>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;">' . $mobile_number . '</p>
+				                                                                    </td>
+				                                                                </tr>
+				                                                                <tr>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;"><b>Location</b></p>
+				                                                                    </td>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;">' . $location . '</p>
+				                                                                    </td>
+				                                                                </tr>
+				                                                                <tr>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;"><b>Need</b></p>
+				                                                                    </td>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;">' . getENQUIRYFORM_NEEDFOR_LIST($need_for, 'label') . '</p>
+				                                                                    </td>
+				                                                                </tr>
+				                                                                <tr>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;"><b>Quantum of Assistance sought</b></p>
+				                                                                    </td>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;">' . $qoas . '</p>
+				                                                                    </td>
+				                                                                </tr>
+				                                                                <tr>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;"><b>Description</b></p>
+				                                                                    </td>
+				                                                                    <td style="border: solid #dddddd 1.0pt; padding: 3.75pt;">
+				                                                                        <p style="margin: 0; margin-bottom: 5.0pt;margin-top: 5.0pt;margin-left:15.0pt;">' . $description . '</p>
+				                                                                    </td>
+				                                                                </tr>
+				                                                            </table>
+				                                                            <div>
+				                                                                <p align="center" style="text-align: center; line-height: 10.5pt;margin-top:15px;">
+				                                                                    <span style="font-size: 10.5pt; color: #4c4d4f;">&copy; ' . date('Y') . ' Small Industries Development Bank of India (SIDBI)</span>
+				                                                                </p>
+				                                                            </div>
+				                                                        </div>
+				                                                    </td>
+				                                                </tr>
+				                                            </table>
+				                                        </div>
+				                                    </body>
+				                                    </html>';
 
                 $subject = "SIDBI : New Online Enquiry";
                 $send_from = $SMTP_EMAIL_SEND_FROM;
